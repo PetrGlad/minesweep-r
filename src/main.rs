6b5cd73fbb170cb602f2a7dc4ec78e4d95929cb1
Petr Gladkikh <petrglad@gmail.com>
2021-03-07 20:44:34 +0100

    Print board
    First version.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-use rand::Rng;
-use rand::prelude::ThreadRng;
 use std::ops::Range;
+
+use ansi_escapes::{ClearScreen, CursorHide, CursorShow, CursorTo};
+use ansi_term::Colour;
+use rand::prelude::ThreadRng;
+use rand::Rng;
 
 type Coord = u16;
 
 #[derive(Debug)]
-#[derive(PartialEq)]
 #[derive(PartialOrd)]
+#[derive(Ord)]
+#[derive(PartialEq)]
+#[derive(Eq)]
 struct Cell(Coord, Coord);
+
+#[derive(Debug)]
+struct Board {
+    ranges: (Range<Coord>, Range<Coord>),
+    cells: Vec<Cell>,
+}
 
 fn new_rnd_cell(rng: &mut ThreadRng, x_range: &Range<Coord>, y_range: &Range<Coord>) -> Cell {
     return Cell(rng.gen_range(x_range.start, x_range.end),
                 rng.gen_range(y_range.start, y_range.end));
+}
+
+fn new_rnd_board(rng: &mut ThreadRng,
+                 rows: &Range<Coord>, cols: &Range<Coord>,
+                 count: u16) -> Board {
+    let mut cells = vec![];
+    for _n in 0..count {
+        cells.push(new_rnd_cell(rng, &cols, &rows));
+    }
+    cells.sort();
+    return Board {
+        ranges: (rows.clone(), cols.clone()),
+        cells,
+    };
+}
+
+fn print_board(board: &Board) {
+    println!("Mines {:?}", board);
+
+    print!("{}{}{}", CursorHide, ClearScreen, CursorTo::TopLeft);
+    for _r in board.ranges.0.clone() {
+        println!("{}", " ".repeat(board.ranges.1.len()));
+    }
+    for cell in &board.cells {
+        print!("{}{} ",
+               CursorTo::AbsoluteXY(cell.0, cell.1 * 2),
+               Colour::Red.paint("*"));
+    }
+    println!("{}{}",
+           CursorTo::AbsoluteXY(board.ranges.0.len() as u16, 0),
+           CursorShow);
 }
 
 fn main() {
@@ -22,6 +64,6 @@ fn main() {
 
     let board_rows = 0..16;
     let board_cols = 0..16;
-    let mines: Vec<Cell> = vec![new_rnd_cell(&mut rng, &board_cols, &board_rows)];
-    println!("Mines {:?}", mines);
+    let mines: Board = new_rnd_board(&mut rng, &board_rows, &board_cols, 12);
+    print_board(&mines);
 }
