d57efe21035e485da6beadf14b478d375b57ea85
Petr Gladkikh <petrglad@gmail.com>
2021-03-21 04:51:35 +0100

    Use regular field representation

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Status: just started. Nothing to see here yet.
 
 # TODO
 
-* Convert storage to arrays, sparse implementation is not strictly needed as we can use mutation now. For optimal presentation would need both sparse and regular presentations, but sparse is more complex, so putting it off until really needed.
+* Convert storage to arrays, sparse implementation is not strictly needed as we can use mutation now. For optimal presentation would need both sparse and regular presentations. Sparse representation is more complex (e.g. hash or 2d tree), so putting it aside until really needed.
 * Consider rust-ndarray/ndarray to process field.
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,57 @@
 use std::ops::Range;
-use std::collections::BTreeSet;
 
 /* TODO: ansi_escapes is unsupported, consider switching
    See https://github.com/LinusU/rust-ansi-escapes/pull/1 */
-use ansi_escapes::{ClearScreen, CursorHide, CursorShow, CursorTo, CursorMove};
+use ansi_escapes::{ClearScreen, CursorHide, CursorShow, CursorTo};
 use ansi_term::Colour;
 use rand::prelude::ThreadRng;
 use rand::Rng;
 
-type Coord = u16;
+type Coord = u8;
 
 #[derive(Debug)]
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 struct Cell(Coord, Coord);
 
-impl Cell {
-    fn random_new(rng: &mut ThreadRng, x_range: &Range<Coord>, y_range: &Range<Coord>) -> Cell {
-        Cell(rng.gen_range(x_range.start, x_range.end),
-             rng.gen_range(y_range.start, y_range.end))
-    }
+type Danger = u8;
+
+const DANGER_NONE: Danger = 0;
+const DANGER_MINE: Danger = 10;
+
+fn is_mine(x: Danger) -> bool {
+    x >= DANGER_MINE
 }
 
 #[derive(Debug)]
 struct Field {
-    ranges: (Range<Coord>, Range<Coord>),
-    cells: Vec<Cell>,
+    cells: Vec<Vec<Danger>>
 }
 
 impl Field {
     fn random_new(rng: &mut ThreadRng,
                   rows: &Range<Coord>,
                   cols: &Range<Coord>,
-                  count: usize) -> Field {
-        assert!(count < (rows.len() * cols.len()));
-        /* Note the following would be slow for dense fields due to duplicates.
-           Need a different algo for that case. E.g. randomly selecting
-           from list of all (x,y) pairs. */
-        let mut cells = BTreeSet::new();
-        while cells.len() < count {
-            cells.insert(Cell::random_new(rng, &cols, &rows));
+                  frac: f32) -> Field {
+        assert!(0.0 <= frac && frac <= 1.0);
+        let mut cells = Vec::with_capacity(rows.len());
+        for _r in rows.start..rows.end {
+            let mut row = Vec::with_capacity(cols.len());
+            for _c in cols.start..cols.end {
+                row.push(if rng.gen::<f32>() < frac { DANGER_MINE } else { DANGER_NONE });
+            }
+            cells.push(row);
         }
         return Field {
-            ranges: (rows.clone(), cols.clone()),
-            cells: cells.into_iter().collect(),
+            cells
         };
     }
 }
 
 #[derive(Debug)]
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone)]
 enum CellState {
+    Unknown,
     // Marked as containing mine
     Marked,
     // Cleared
@@ -57,23 +59,18 @@ enum CellState {
 }
 
 #[derive(Debug)]
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-struct BoardCell {
-    cell: Cell,
-    state: CellState,
-}
-
-#[derive(Debug)]
 struct Board {
-    ranges: (Range<Coord>, Range<Coord>),
-    cells: Vec<BoardCell>,
+    cells: Vec<Vec<CellState>>
 }
 
 impl Board {
-    fn new(x_range: &Range<Coord>, y_range: &Range<Coord>) -> Board {
+    fn new(rows: &Range<Coord>, cols: &Range<Coord>) -> Board {
+        let mut cells = Vec::with_capacity(rows.len());
+        for _r in rows.start..rows.end {
+            cells.push(vec![CellState::Unknown; cols.len()]);
+        }
         Board {
-            ranges: (x_range.clone(), y_range.clone()),
-            cells: Vec::new(),
+            cells
         }
     }
 }
@@ -84,20 +81,15 @@ trait Paint {
 
 impl Paint for Field {
     fn print(&self) {
-        let mut ci = self.cells.iter();
-        let mut cell = ci.next().unwrap();
-        for row in self.ranges.0.clone() {
-            let mut col = 0;
-            while cell.0 <= row {
-                print!("{}{}",
-                       CursorMove::X((cell.1 - col) as i16 * 2),
-                       Colour::Red.paint("*"));
-                let o_cell = ci.next();
-                if o_cell.is_none() {
-                    break;
+        for row in &self.cells {
+            for cell in row {
+                if is_mine(*cell) {
+                    print!("{} ", Colour::Red.paint("*"))
+                } else if *cell == 0u8 {
+                    print!("  ");
+                } else {
+                    print!("{:1} ", cell);
                 }
-                col = cell.1;
-                cell = o_cell.unwrap();
             }
             println!();
         }
@@ -107,24 +99,14 @@ impl Paint for Field {
 // TODO Unify print code with Field.
 impl Paint for Board {
     fn print(&self) {
-        let mut ci = self.cells.iter();
-        let cell1 = ci.next();
-        if cell1.is_none() {
-            return
-        }
-        let mut b_cell = cell1.unwrap();
-        for row in self.ranges.0.clone() {
-            let mut col = 0;
-            while b_cell.cell.0 <= row {
-                print!("{}{}",
-                       CursorMove::X((b_cell.cell.1 - col) as i16 * 2),
-                       Colour::Red.paint("*"));
-                let o_cell = ci.next();
-                if o_cell.is_none() {
-                    break;
-                }
-                col = b_cell.cell.1;
-                b_cell = o_cell.unwrap();
+        for row in &self.cells {
+            for cell in row {
+                print!("{} ",
+                       match cell {
+                           CellState::Free => Colour::White.paint(" "),
+                           CellState::Marked => Colour::Yellow.paint("@"),
+                           _ => Colour::Black.paint("#")
+                       });
             }
             println!();
         }
@@ -137,10 +119,10 @@ fn main() {
     assert!(Cell(1, 3) < Cell(2, 3));
     assert!(Cell(1, 2) < Cell(1, 3));
 
-    let board_rows = 0..16;
-    let board_cols = 0..16;
+    let board_rows = 0..15;
+    let board_cols = 0..15;
 
-    let mines: Field = Field::random_new(&mut rng, &board_rows, &board_cols, 100);
+    let mines: Field = Field::random_new(&mut rng, &board_rows, &board_cols, 0.12);
     let board: Board = Board::new(&board_rows, &board_cols);
 
     print!("{}{}{}", CursorHide, ClearScreen, CursorTo::TopLeft);
